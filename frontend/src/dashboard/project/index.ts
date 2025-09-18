e6afb933d34d74a84f1d9d377faa95c9e2acf1dd
Jaz Belrose <jazbelrose@icloud.com>
2025-09-17 23:23:14 -0700

    refactor: update import paths for DesignerComponent, MessageItem, PreviewDrawer, and ProjectMessagesThread for consistency

--- frontend/src/dashboard/project/index.ts
+++ frontend/src/dashboard/project/index.ts
@@ -6,12 +6,12 @@ export { default as SingleProjectPage } from "./pages/project";
 // Components
 export { default as ChatPanel } from "./components/ChatPanel";
 export { default as ClientInvoicePreviewModal } from "./components/ClientInvoicePreviewModal";
-export { default as DesignerComponent } from "../editor/components/canvas/designercomponent";
+export { default as DesignerComponent } from "./features/editor/components/canvas/designercomponent";
 export { default as Dropdown } from "./components/Dropdown";
 export { default as FileManager } from "./components/FileManager";
 export { default as GalleryComponent } from "./components/GalleryComponent";
 export { default as LocationComponent } from "./components/LocationComponent";
-export { default as MessageItem } from "../messages/MessageItem";
+export { default as MessageItem } from "../features/messages/MessageItem";
 export { default as NewProjectAddress } from "./components/NewProjectAddress";
 export { default as NewProjectBudget } from "./components/NewProjectBudget";
 export { default as NewProjectDescription } from "./components/NewProjectDescription";
@@ -20,10 +20,10 @@ export { default as NewProjectHeader } from "./components/NewProjectHeader";
 export { default as NewProjectName } from "./components/NewProjectName";
 export { default as NewProjectUploadFiles } from "./components/NewProjectUploadFiles";
 export { default as PDFPreview } from "./components/PDFPreview";
-export { default as PreviewDrawer } from "../editor/components/PreviewDrawer";
+export { default as PreviewDrawer } from "./features/editor/components/PreviewDrawer";
 export { default as ProjectCalendar } from "./components/ProjectCalendar";
 export { default as ProjectHeader } from "./components/ProjectHeader";
-export { default as ProjectMessagesThread } from "../messages/ProjectMessagesThread";
+export { default as ProjectMessagesThread } from "../features/messages/ProjectMessagesThread";
 export { default as ProjectPageLayout } from "./components/ProjectPageLayout";
 
 export { default as QuickLinksComponent } from "./components/QuickLinksComponent";
